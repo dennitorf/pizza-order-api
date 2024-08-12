027c61e2ba22d579ca770a24962d4078c34b5aa7
henrry <henamorado29102@gmail.com>
2024-08-12 09:06:06 -0400

    inserting data

--- Src/Restaurant.Pizza.Persistence/Contexts/AppDbContext.cs
+++ Src/Restaurant.Pizza.Persistence/Contexts/AppDbContext.cs
@@ -16,8 +16,12 @@ namespace Restaurant.Pizza.Persistence.Contexts
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         }        
         public DbSet<Todo> Todos { set; get; }
-        public DbSet<TodoItem> TodoItems { set; get; }
-
+        public DbSet<TodoItem> TodoItems { set; get; }        
+        public DbSet<Size> Sizes { set; get; }
+        public DbSet<ToppingType> ToppingTypes { set; get; }
+        public DbSet<Topping> Toppings { set; get; }
+        public DbSet<Offer> Offers { set; get; }
         public DbSet<Pizza.Domain.Entities.Pizza> Piza { set; get; }
+
     }
 }
